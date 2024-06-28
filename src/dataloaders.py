31dedbc3da3fe692812f64611adab1a510bdaaa8
Rasmus <r.juul97@gmail.com>
2024-06-28 13:19:09 +0200

    renamed models to checkpoints and added missing files

---
+++ src/dataloaders.py
@@ -0,0 +1,231 @@
+# -*- coding: utf-8 -*-
+import os
+
+import torch
+import numpy as np
+import pytorch_lightning as pl
+
+from tqdm import tqdm
+from glob import glob
+from tifffile import tifffile
+from torch.utils.data import DataLoader
+
+from src import _PATH_DATA
+
+
+class Geometry(torch.nn.Module):
+    def __init__(self,source_pos,detector_pos,detector_size,detector_pixel_size,object_shape,beam_type="cone"):
+        """
+        Parameters
+        ----------
+        source_pos : torch.Tensor
+            ray source position x,y,z position in relation to sample
+        detector_pos : torch.Tensor
+            center of detector x,y,z position in relation to sample
+        detector_size : Sequence[int]
+            Shape of detector in pixels e.g. (300,300)
+        detector_pixel_size : Sequence[torch.Tensor]
+            Two vectors defining the size of the pixels 
+            u : the vector from detector pixel (0,0) to (0,1)
+            v : the vector from detector pixel (0,0) to (1,0)
+        object_shape: Sequence[int]
+            shape of object in um, i.e. an object which has the dimensions 200x200x300um has the shape [200,200,300]
+        beam_type : str
+            Which beam type to use for calculating projection. Default 'cone'
+        """
+        super(Geometry, self).__init__()
+        self.source_pos = source_pos
+        self.detector_pos = detector_pos
+        self.detector_size = detector_size
+        self.detector_pixel_size = detector_pixel_size
+        self.u_vec = detector_pixel_size[:3]
+        self.v_vec = detector_pixel_size[3:]
+        self.beam_type = beam_type
+        self.object_shape = object_shape
+
+        self.detector_coordinates()
+        self.ray_directions()
+        self.min_depth = -source_pos[2]-object_shape[2]/2
+        self.max_depth = self.min_depth + object_shape[2]
+        self.start_points = (self.source_pos/(self.object_shape[2]/2) + self.rays * self.min_depth/(self.object_shape[2]/2)) 
+        self.end_points =  self.source_pos/(self.object_shape[2]/2) + self.rays * self.max_depth/(self.object_shape[2]/2)
+
+    def detector_coordinates(self):
+        x_component = self.u_vec*(self.detector_size[0]/2)
+        y_component = self.v_vec*(self.detector_size[1]/2)
+        
+        bottom_left_corner = self.detector_pos - x_component - y_component
+        bottom_left_corner_pixel_pos = bottom_left_corner + self.u_vec/6 + self.v_vec/6 #Something weird here I would expect it to be self.u_vec/2 + self.v_vec/2
+
+        detector_pixel_coordinates = torch.zeros((self.detector_size[0],self.detector_size[1],3))
+        if self.detector_size[0] == self.detector_size[1]:
+            for i in range(self.detector_size[0]):
+                detector_pixel_coordinates[:,i,0] = torch.arange(0,self.detector_size[0])
+                detector_pixel_coordinates[i,:,1] = torch.arange(0,self.detector_size[1])
+        else:
+            for i in range(self.detector_size[1]):
+                detector_pixel_coordinates[:,i,0] = torch.arange(0,self.detector_size[0])
+            for i in range(self.detector_size[0]):
+                detector_pixel_coordinates[i,:,1] = torch.arange(0,self.detector_size[1])
+        
+        self.detector_pixel_coordinates = (detector_pixel_coordinates * self.u_vec + detector_pixel_coordinates * self.v_vec)+bottom_left_corner_pixel_pos
+    
+    def ray_directions(self):
+        self.rays = (self.detector_pixel_coordinates - self.source_pos)
+        lengths = torch.linalg.norm(self.rays,dim=2)
+        self.rays[:,:,0] = self.rays[:,:,0] / lengths
+        self.rays[:,:,1] = self.rays[:,:,1] / lengths
+        self.rays[:,:,2] = self.rays[:,:,2] / lengths
+    
+    def sample_points(self,num_points,noise=False):
+        """
+        Parameters
+        ----------
+        num_points : int
+            Number of points sampled per ray 
+        """
+        self.step_size = (self.end_points-self.start_points)/num_points
+        points = torch.zeros(self.detector_size[0],self.detector_size[1],num_points+1,3)
+        for i in range(num_points+1):
+            points[:,:,i,:] = self.start_points + self.step_size*i
+
+
+        self.points = points.view(-1,num_points+1,3)
+        
+
+class CTpoints(torch.utils.data.Dataset):
+    def __init__(self, args_dict):
+
+        self.args = args_dict
+        data_path = self.args['general']['data_path'] 
+        
+        positions = np.load(f"{data_path}_positions.npy")
+        projections = np.load(f"{data_path}_projections.npy")
+        projections = np.swapaxes(projections,1,0)
+        projections = np.swapaxes(projections,1,2)
+        projections = 1 - np.exp(-projections/((projections.shape[1]+1)/2)) #In astra projection is just the sum of attenuation along the ray into the pixel, so computing the intensity at the detector using the Beer-Lambert Law
+        self.projections = projections
+        
+        img = torch.tensor(tifffile.imread(f"{args_dict['general']['data_path']}.tif"))
+        img -= img.min()
+        img = img/img.max()
+        self.img = img.permute(2,1,0)
+        if self.args['training']['imagefit_mode']:
+            mgrid = torch.stack(torch.meshgrid(torch.linspace(-1, 1, self.img.shape[0]),
+                                               torch.linspace(-1, 1, self.img.shape[1]),
+                                               torch.linspace(-1, 1, self.img.shape[2]),
+                                               indexing='ij'),
+                                dim=-1)
+            self.mgrid = mgrid.view(-1,self.img.shape[2],3)
+        
+        
+        self.detector_size = self.projections[0,:,:].shape
+        detector_pos = torch.tensor(positions[:,3:6])
+        detector_pixel_size = torch.tensor(positions[:,6:])
+        
+        source_pos = torch.tensor(positions[:,:3])
+        object_shape = self.img.shape
+        
+        self.geometries = [None]*positions.shape[0]
+        
+        for i in tqdm(range(positions.shape[0]),desc='Generating points from rays'):
+            self.geometries[i] = Geometry(source_pos[i],detector_pos[i],self.detector_size,detector_pixel_size[i],object_shape)
+            self.geometries[i].sample_points(self.args['training']['num_points'])
+        
+        self.pose_idx = 0
+    
+    def __len__(self):
+        if self.args['training']['imagefit_mode']:
+            return self.img.shape[2]
+        else:
+            return self.detector_size[0]*self.detector_size[1]
+
+    def set_pose_idx(self, idx):
+        self.pose_idx = idx
+
+    def __getitem__(self, idx):
+        if self.args['training']['imagefit_mode']:
+            x = self.mgrid[:,idx,:]
+            y = self.img[:,:,idx]
+            step_size = None
+        else:
+            x = self.geometries[self.pose_idx].points[idx].view(-1,3)
+            y = self.projections[self.pose_idx,:,:].flatten()[idx]
+            step_size = self.geometries[self.pose_idx].step_size.view(-1,3)[idx]
+            
+        return x,y,step_size
+
+class CTDataModule(pl.LightningDataModule):
+    def __init__(
+        self,
+        args_dict,
+        num_poses = 16,
+    ):
+        """
+        Initializes the BugNISTDataModule.
+
+        Parameters
+        ----------
+        args_dict : Dict
+            Dictionary with arguments
+        num_poses : int
+            Number of poses
+        """
+        super().__init__()
+        self.args = args_dict
+        self.num_poses = num_poses
+
+    def setup(self, stage=None):
+        """
+        Setup datasets for different stages (e.g., 'fit', 'test').
+
+        Args:
+        - stage (str, optional): The stage for which data needs to be set up. Defaults to None.
+        """
+        self.dataset = CTpoints(self.args)
+        # if stage == "test" or stage is None:
+        # if stage == "fit" or stage is None:
+            
+
+    def train_dataloader(self,shuffle=True):
+        """
+        Returns the training data loader.
+        """
+        self.dataset.set_pose_idx(self.trainer.current_epoch%self.num_poses)
+        return DataLoader(
+            self.dataset,
+            batch_size=self.args["training"]["batch_size"],
+            num_workers=self.args["training"]["num_workers"],
+            pin_memory=True,
+            shuffle=shuffle,
+        )
+
+    def val_dataloader(self):
+        """
+        Returns the validation data loader.
+        """
+        self.dataset.set_pose_idx(self.trainer.current_epoch%self.num_poses)
+        return DataLoader(
+            self.dataset,
+            batch_size=self.args["training"]["batch_size"],
+            num_workers=self.args["training"]["num_workers"],
+            pin_memory=True,
+        )
+
+    def test_dataloader(self):
+        """
+        Returns the test data loader.
+        """
+        return DataLoader(
+            self.dataset,
+            batch_size=self.args["training"]["batch_size"],
+            num_workers=self.args["training"]["num_workers"],
+            pin_memory=True,
+        )
+    def dataloader_for_testing(self):
+        return DataLoader(
+            self.dataset,
+            batch_size=self.args["training"]["batch_size"],
+            num_workers=self.args["training"]["num_workers"],
+            pin_memory=True,
+        )
---
+++ src/models/mlp.py
@@ -0,0 +1,306 @@
+import torch
+import numpy as np
+from pytorch_lightning import LightningModule
+import torchmetrics as tm
+import tifffile
+from tqdm import tqdm
+
+def get_activation_function(activation_function,**kwargs):
+    if activation_function == 'relu':
+        return torch.nn.ReLU(**kwargs)
+    elif activation_function == 'leaky_relu':
+        return torch.nn.LeakyReLU(**kwargs)
+    elif activation_function == 'sigmoid':
+        return torch.nn.Sigmoid(**kwargs)
+    elif activation_function == 'tanh':
+        return torch.nn.Tanh(**kwargs)
+    elif activation_function == 'elu':
+        return torch.nn.ELU(**kwargs)
+    elif activation_function == 'none':
+        return torch.nn.Identity(**kwargs)
+    elif activation_function == 'sine':
+        return Sine(**kwargs)
+    else:
+        raise ValueError(f"Unknown activation function: {activation_function}")
+
+class TrigonometricEncoder(torch.nn.Module):
+    
+    def __init__(self,num_freq_bands):
+        super(TrigonometricEncoder,self).__init__()
+        
+        self.num_freq_bands = num_freq_bands
+        
+    def forward(self,idxs):
+        idxs = idxs.T
+        enc = [idxs]
+        for i in range(self.num_freq_bands):
+            enc.append(torch.sin(2**i *torch.pi*idxs))
+            enc.append(torch.cos(2**i *torch.pi*idxs))
+            
+        enc = torch.cat(enc)
+        
+        return enc.T
+        
+class Sine(torch.nn.Module):
+    def __init(self):
+        super().__init__()
+
+    def forward(self, input):
+        # See Siren paper sec. 3.2, final paragraph, and supplement Sec. 1.5 for discussion of factor 30
+        return torch.sin(30 * input)
+
+
+def sine_init(m):
+    with torch.no_grad():
+        if hasattr(m, 'weight'):
+            num_input = m.weight.size(-1)
+            # In siren paper see supplement Sec. 1.5 for discussion of factor 30
+            m.weight.uniform_(-np.sqrt(6 / num_input) / 30, np.sqrt(6 / num_input) / 30)
+
+
+def first_layer_sine_init(m):
+    with torch.no_grad():
+        if hasattr(m, 'weight'):
+            num_input = m.weight.size(-1)
+            m.weight.uniform_(-1 / num_input, 1 / num_input)
+
+
+class MLP(LightningModule):
+
+    def __init__(self,args_dict,projection_shape):
+        super(MLP,self).__init__()
+        self.save_hyperparameters()
+
+        self.projection_shape = projection_shape
+        self.lr = args_dict['training']['learning_rate']
+        self.imagefit_mode = args_dict["training"]["imagefit_mode"]
+        self.noisy = args_dict['training']['noisy_points']
+
+        self.l1_regularization_weight = 1
+
+        if self.imagefit_mode:
+            self.img_shape = tifffile.imread(f"{args_dict['general']['data_path']}.tif").shape
+
+        self.num_freq_bands = args_dict['model']['num_freq_bands']
+        self.num_hidden_layers = args_dict['model']['num_hidden_layers']
+        self.num_hidden_features = args_dict['model']['num_hidden_features']
+        self.activation_function = args_dict['model']['activation_function']
+
+        if self.num_freq_bands > 0:
+            self.trig_encoder = TrigonometricEncoder(num_freq_bands=self.num_freq_bands)
+            num_input_features = 3 * (self.num_freq_bands * 2 + 1) # +1 for the constant (original input)
+        else:
+            num_input_features = 3 # x,y,z coordinate
+
+
+        layers = []
+        for i in range(self.num_hidden_layers):
+            layers.append(torch.nn.Sequential(torch.nn.Linear(self.num_hidden_features,self.num_hidden_features),
+                                         get_activation_function(self.activation_function),
+                                         ))
+
+
+        self.mlp = torch.nn.Sequential(torch.nn.Linear(num_input_features,self.num_hidden_features),
+                                       get_activation_function(self.activation_function),
+                                       *layers,
+                                        torch.nn.Linear(self.num_hidden_features,1),
+                                        )
+
+        if self.activation_function == 'sine':
+            self.mlp.apply(sine_init)
+            self.mlp[0].apply(first_layer_sine_init)
+
+        self.loss_fn = torch.nn.MSELoss()
+        self.l1_regularization = torch.nn.L1Loss()
+        self.psnr = tm.image.PeakSignalNoiseRatio()
+        self.validation_step_outputs = []
+        self.validation_step_gt = []
+        
+    @torch.jit.script
+    def compute_projection_values(num_points: int,
+                                  attenuation_values: torch.Tensor
+                                 ) -> torch.Tensor:
+        I0 = 1
+        # Compute the spacing between ray points
+        dx = 2 / (num_points)
+
+        # Compute the sum of mu * dx along each ray
+        attenuation_sum = torch.sum(attenuation_values * dx, dim=1)
+    
+        # Compute the intensity at the detector using the Beer-Lambert Law
+        intensity = I0 * torch.exp(-attenuation_sum)
+        
+        # Inverse the intensity to make it look like CT
+        return I0-intensity
+
+    
+    def forward(self, pts):
+        pts_shape = pts.shape
+
+        if len(pts.shape) > 2:
+            pts = pts.view(-1,3)
+
+        if self.num_freq_bands > 0:
+            enc = self.trig_encoder(pts)
+        else:
+            enc = pts
+
+        out = self.mlp(enc)
+
+        if len(pts_shape) > 2:
+            out = out.view(*pts_shape[:-1],-1)
+
+        return out
+
+    def training_step(self, batch, batch_idx):
+        # training_step defines the train loop.
+        points, target, step_size = batch
+        points_dtype = points.dtype
+
+        if self.imagefit_mode:
+            attenuation_values = self.forward(points)
+            attenuation_values = attenuation_values.view(target.shape)
+            loss = self.loss_fn(attenuation_values,target)
+        else:
+            if self.noisy:
+                noise = torch.zeros_like(step_size,device=points.device)
+                noise[:,0] = (torch.rand(noise[:,0].shape,device=points.device)-0.5)*0.1
+                noise[:,1] = (torch.rand(noise[:,1].shape,device=points.device)-0.5)*0.1
+                noise[:,2] = torch.rand(noise[:,2].shape,device=points.device)-0.5
+                points = points+(noise*step_size)[:,None,:]
+                points = points.to(points_dtype)
+
+            
+            attenuation_values = self.forward(points.view(-1,3)).view(points.shape[0],points.shape[1])
+            detector_value_hat = self.compute_projection_values(points.shape[1],attenuation_values)
+
+            smoothness_loss = self.l1_regularization(attenuation_values[:,1:],attenuation_values[:,:-1])
+            loss = self.loss_fn(detector_value_hat, target)
+
+            total_loss = loss + self.l1_regularization_weight*smoothness_loss
+        self.log_dict(
+            {
+                "train/loss": loss,
+                "train/loss_total":total_loss,
+                "train/l1_regularization":smoothness_loss,
+            },
+            on_step=False,
+            on_epoch=True,
+        )
+
+        return total_loss
+
+    def validation_step(self, batch, batch_idx):
+        points, target, _ = batch
+
+        if self.current_epoch % 5 == 0:
+            if self.imagefit_mode:
+                attenuation_values = self.forward(points)
+                attenuation_values = attenuation_values.view(target.shape)
+                
+                loss = self.loss_fn(attenuation_values,target)
+                
+                self.validation_step_outputs.append(attenuation_values)
+                self.validation_step_gt.append(target)
+            else:
+                attenuation_values = self.forward(points.view(-1,3)).view(points.shape[0],points.shape[1])
+                detector_value_hat = self.compute_projection_values(points.shape[1],attenuation_values)
+
+                smoothness_loss = self.l1_regularization(attenuation_values[:,1:],attenuation_values[:,:-1])
+        
+                loss = self.loss_fn(detector_value_hat, target)
+                
+                self.validation_step_outputs.append(detector_value_hat)
+                self.validation_step_gt.append(target)
+
+                total_loss = loss + self.l1_regularization_weight*smoothness_loss
+    
+            self.log_dict(
+                {
+                    "val/loss": loss,
+                    "val/loss_total":total_loss,
+                    "val/l1_regularization":smoothness_loss,
+                },
+                on_step=False,
+                on_epoch=True,
+            )
+        
+
+    def on_validation_epoch_end(self):
+        if self.current_epoch % 5 == 0:
+            all_preds = torch.cat(self.validation_step_outputs)
+            all_gt = torch.cat(self.validation_step_gt)
+            if self.imagefit_mode:
+                preds = all_preds.view(self.img_shape)
+                gt = all_gt.view(self.img_shape)
+                self.logger.log_image(key="val/reconstruction", images=[preds[self.img_shape[2]//2,:,:], gt[self.img_shape[2]//2,:,:]], caption=["pred", "gt"]) # log projection images
+                psnr = self.psnr(preds.unsqueeze(dim=0).unsqueeze(dim=0),gt.unsqueeze(dim=0).unsqueeze(dim=0))
+                self.log("val/reconstruction",psnr)
+            else:
+                preds = all_preds.view(self.projection_shape)
+                gt = all_gt.view(self.projection_shape)
+        
+                self.logger.log_image(key="val/projection", images=[preds, gt], caption=["pred", "gt"]) # log projection images
+                psnr = self.psnr(preds.unsqueeze(dim=0).unsqueeze(dim=0),gt.unsqueeze(dim=0).unsqueeze(dim=0))
+                self.log("val/psnr_projection",psnr)
+
+                img = self.trainer.val_dataloaders.dataset.img
+                
+                mgrid = torch.stack(torch.meshgrid(torch.linspace(-1, 1, img.shape[0]), torch.linspace(-1, 1, img.shape[1]), torch.linspace(-1, 1, img.shape[2]), indexing='ij'),dim=-1)
+                mgrid = mgrid.view(-1,img.shape[2],3)
+                outputs = torch.zeros((*mgrid.shape[:2],1))
+                for i in range(mgrid.shape[1]):
+                    output = self.forward(mgrid[:,i,:].to(device=self.device))
+                    
+                    outputs[:,i,:] = output.cpu()
+
+                outputs = outputs.view(img.shape)
+                self.log("val/psnr_reconstruction",self.psnr(outputs.unsqueeze(dim=0).unsqueeze(dim=0),img.unsqueeze(dim=0).unsqueeze(dim=0)))
+                self.log("val/loss_reconstruction",self.loss_fn(outputs,img))
+                self.logger.log_image(key="val/reconstruction", images=[outputs[img.shape[2]//2,:,:], img[img.shape[2]//2,:,:],
+                                                                        outputs[:,img.shape[2]//2,:], img[:,img.shape[2]//2,:],
+                                                                        outputs[:,:,img.shape[2]//2], img[:,:,img.shape[2]//2]],
+                                      caption=["pred_xy", "gt_xy",
+                                               "pred_yz", "gt_yz",
+                                               "pred_xz", "gt_xz"])
+                
+            
+            self.validation_step_outputs.clear()  # free memory
+            self.validation_step_gt.clear()  # free memory
+
+    def test_step(self, batch, batch_idx):
+        points, target, _ = batch
+
+        if self.imagefit_mode:
+            attenuation_values = self.forward(points)
+            attenuation_values = attenuation_values.view(target.shape)
+            
+            loss = self.loss_fn(attenuation_values,target)
+        else:
+            attenuation_values = self.forward(points.view(-1,3)).view(points.shape[0],points.shape[1])
+            detector_value_hat = self.compute_projection_values(points.shape[1],attenuation_values)
+    
+            loss = self.loss_fn(detector_value_hat, target)
+
+        self.log_dict(
+            {
+                "test/loss": loss,
+            },
+            on_step=False,
+            on_epoch=True,
+            sync_dist=True,
+        )
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+        # return optimizer
+        lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=100, factor=0.5, cooldown=1)
+        lr_scheduler_config = {
+            "scheduler": lr_scheduler,
+            "interval": "epoch",
+            "monitor": "val/loss_total",}
+        return {"optimizer": optimizer, "lr_scheduler": lr_scheduler_config}
+        
+
+
+
